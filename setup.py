056cd699030e2b14918139c24996de0fee619d63
Mason <mason.shipton@ontariotechu.net>
2024-12-22 14:52:15 -0500

    Release v1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="urielplus",
-    version="1.0",
+    version="1.1",
     author="Mason Shipton",
     author_email="masonshipton25@gmail.com",
     description="URIEL+: Knowledge base for natural language processing",
